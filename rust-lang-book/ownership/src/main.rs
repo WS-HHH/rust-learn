e9b334be5e99987a987b7f8141b743cca25947f0
ss <sswang0x@gmail.com>
2023-11-11 15:17:28 +0800

    feat: reference

--- rust-lang-book/ownership/src/main.rs
+++ rust-lang-book/ownership/src/main.rs
@@ -20,12 +20,29 @@ fn main() {
   // println!("The value of s3 is: {}", s3);
 
   // 返回参数的所有权示例
-  let s1 = String::from("hello");
+  // let s1 = String::from("hello");
 
-  let (s2, length) = calculate_value(s1);
+  // let (s2, length) = calculate_value(s1);
 
-  println!("The value of s2 and length is: {},{}", s2, length);
+  // println!("The value of s2 and length is: {},{}", s2, length);
 
+  // 引用示例
+  // let s1 = String::from("hello");
+  // let len = calculate_len(&s1);
+  // println!("The value of is {}", len);
+
+  // 可变引用示例
+  // let mut s1 = String::from("hello");
+  // change(&mut s1);
+  // println!("The value of is {}", s1);
+
+  // 如果对某变量进行了可变引用，无法同时再对改变量进行引用
+  // let mut s1 = String::from("hello");
+  // let s2 = &mut s1;
+  // let s3 = &mut s1; // 不可用
+
+  
+  // println!("The value of is s2 and s3, {}, {}", s2, s3);
 }
 
 // fn gives_ownership() -> String {
@@ -45,3 +62,12 @@ fn main() {
 //   (s1, length)
 
 // }
+
+// fn calculate_len(s1: &String) -> usize {
+//   s1.len()
+// }
+
+// 可变引用
+fn change(s1: &mut String) {
+  s1.push_str(", world");
+}
